b325d26325e9369dbda38aa25056d770f2bdfc44
Armando Lima <113323844+armando-lima@users.noreply.github.com>
2023-10-15 00:15:57 -0400

    Add function to create divs

--- javascript.js
+++ javascript.js
@@ -1,5 +1,22 @@
-const gridSizeSlider = document.querySelector("input");
+const gridSizeSlider = document.querySelector(".slider");
 let sliderLabel = document.querySelector(".sliderLabel");
+const gridContainer = document.querySelector(".sketchContainer");
+
 gridSizeSlider.addEventListener("input", () => {
-    sliderLabel.innerText = gridSizeSlider.value;
-})
+    let gridSizeValue = gridSizeSlider.value;
+    sliderLabel.innerText = gridSizeValue;
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    for (i = 1; i <= gridSizeValue; i++){
+        let gridDraw = document.createElement("div");
+        gridDraw.classList.add("sketchArea");
+        gridDraw.innerText = i;
+        gridContainer.appendChild(gridDraw);
+    }
+});
+
+
+
+
+
