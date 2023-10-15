df7032f9c2793f5470cf9ba1fc81ee4a8c119965
Armando Lima <113323844+armando-lima@users.noreply.github.com>
2023-10-15 18:51:46 -0400

    Fix function to create div matrix
    Split function to create grid in two:
    1 - input event that sets the slider label
    2 - mouseup event that creates the matrix of divs
    
    This was done due to performance issues. If leaving the creation
    of the matrix inside the input event, the calculations for height
    and width would be in real time, lagging the application.

--- javascript.js
+++ javascript.js
@@ -5,18 +5,21 @@ const gridContainer = document.querySelector(".sketchContainer");
 gridSizeSlider.addEventListener("input", () => {
     let gridSizeValue = gridSizeSlider.value;
     sliderLabel.innerText = gridSizeValue;
+});
+
+gridSizeSlider.addEventListener("mouseup", () => {
+    let gridSizeValue = gridSizeSlider.value;
+    let gridWidth = gridContainer.offsetWidth / gridSizeValue;
+    let gridHeight = gridContainer.offsetHeight / gridSizeValue;
     while (gridContainer.hasChildNodes()) {
         gridContainer.removeChild(gridContainer.lastChild);
     }
-    for (i = 1; i <= gridSizeValue; i++){
+    for (i = 1; i <= gridSizeValue * gridSizeValue; i++) {
         let gridDraw = document.createElement("div");
         gridDraw.classList.add("sketchArea");
         gridDraw.innerText = i;
+        gridDraw.style.width = gridWidth + "px";
+        gridDraw.style.height = gridHeight + "px";
         gridContainer.appendChild(gridDraw);
     }
 });
-
-
-
-
-
